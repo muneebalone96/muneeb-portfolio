7bb18c7d183b44e96a6c1d3ef15cdf52ecdf81b0
muneebalone96 <muneebalone96@gmail.com>
2026-09-01 23:41:24 -0700

    Fix portfolio sections by loading original script

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// GitHub Pages compatibility loader: the original script.js is stored at the repository root.
+(function(){
+  var s = document.createElement('script');
+  s.src = '../script.js';
+  s.async = false;
+  document.head.appendChild(s);
+})();
